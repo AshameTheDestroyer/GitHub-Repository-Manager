d594c9798093d9c0265df51bc8f758b71688d197
Ashame The Destroyer <hashemwnoos@gmail.com>
2023-03-16 00:47:30 +0200

    deployment

--- src/Page.jsx
+++ src/Page.jsx
@@ -20,19 +20,21 @@ function Page() {
     const [state, setState] = useState(INITIAL_STATE);
 
     return (
-        <stateContext.Provider value={{ state, setState }}>
-            <section>
+        <BrowserRouter basename={window.location.pathname || ''}>
+            <stateContext.Provider value={{ state, setState }}>
+                <section>
 
-                <Route path='/Header/Header'>
-                    <Header title="GitHub Repository Manager" />
-                </Route>
+                    <Route path='/Header/Header'>
+                        <Header title="GitHub Repository Manager" />
+                    </Route>
 
-                <Route path='MainSection/MainSection'>
-                    <MainSection />
-                </Route>
+                    <Route path='MainSection/MainSection'>
+                        <MainSection />
+                    </Route>
 
-            </section>
-        </stateContext.Provider>
+                </section>
+            </stateContext.Provider>
+        </BrowserRouter>
     );
 }
 
