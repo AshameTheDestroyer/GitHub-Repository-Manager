314f2301e1e39b5612a96c2fbee69c647f2e74f0
Ashame The Destroyer <hashemwnoos@gmail.com>
2023-03-15 20:42:04 +0200

    deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Link } from 'react-router-dom';
 import Icon from './Icon/icon';
 import './index.scss';
 import './main.scss';
@@ -10,15 +10,17 @@ import github_icon from "./images/Icons/github_large.png";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <HashRouter basename="/">
-        <main>
-            <Icon iconURL={github_icon} size="25vmin" isButton={true} link="https://www.github.com/" />
-            <h1>GitHub Repository Manager</h1>
-            <p>Review all information about your GitHub account!</p>
-            <button>
-                <p>Get Started</p>
-                <a href="../Page.html"></a>
-            </button>
-        </main>
+        <Link to="https://AshameTheDestroyer.github.io/GitHub-Repository-Manager/">
+            <main>
+                <Icon iconURL={github_icon} size="25vmin" isButton={true} link="https://www.github.com/" />
+                <h1>GitHub Repository Manager</h1>
+                <p>Review all information about your GitHub account!</p>
+                <button>
+                    <p>Get Started</p>
+                    <a href="../Page.html"></a>
+                </button>
+            </main>
+        </Link>
     </HashRouter>
 );
 
