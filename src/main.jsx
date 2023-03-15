9e6f8e289c858d2d319a7826c3f48f51617f32cd
Ashame The Destroyer <hashemwnoos@gmail.com>
2023-03-16 00:42:36 +0200

    deployment

--- src/main.jsx
+++ src/main.jsx
@@ -9,15 +9,17 @@ import github_icon from "./images/Icons/github_large.png";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <main>
-        <Icon iconURL={github_icon} size="25vmin" isButton={true} link="https://www.github.com/" />
-        <h1>GitHub Repository Manager</h1>
-        <p>Review all information about your GitHub account!</p>
-        <button>
-            <p>Get Started</p>
-            <Link to="../Page.html"></Link>
-        </button>
-    </main>
+    <BrowserRouter basename={window.location.pathname || ''}>
+        <main>
+            <Icon iconURL={github_icon} size="25vmin" isButton={true} link="https://www.github.com/" />
+            <h1>GitHub Repository Manager</h1>
+            <p>Review all information about your GitHub account!</p>
+            <button>
+                <p>Get Started</p>
+                <Link to="../Page.html"></Link>
+            </button>
+        </main>
+    </BrowserRouter>
 );
 
 export default root;
