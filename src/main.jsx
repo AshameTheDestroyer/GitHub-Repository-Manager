4bb29ddc6f95d9768d2e5e70a901a7aebe49f663
Ashame The Destroyer <hashemwnoos@gmail.com>
2023-03-16 00:19:15 +0200

    deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Icon from './Icon/icon';
 import './index.scss';
 import './main.scss';
@@ -9,19 +9,20 @@ import github_icon from "./images/Icons/github_large.png";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter basename="/">
-        <Link to="https://AshameTheDestroyer.github.io/GitHub-Repository-Manager/">
-            <main>
-                <Icon iconURL={github_icon} size="25vmin" isButton={true} link="https://www.github.com/" />
-                <h1>GitHub Repository Manager</h1>
-                <p>Review all information about your GitHub account!</p>
-                <button>
-                    <p>Get Started</p>
-                    <a href="../Page.html"></a>
-                </button>
-            </main>
-        </Link>
-    </HashRouter>
+    <BrowserRouter basename={window.location.pathname || ''}>
+        <Routes>
+            <Route exact path="/" />
+        </Routes>
+        <main>
+            <Icon iconURL={github_icon} size="25vmin" isButton={true} link="https://www.github.com/" />
+            <h1>GitHub Repository Manager</h1>
+            <p>Review all information about your GitHub account!</p>
+            <button>
+                <p>Get Started</p>
+                <a href="../Page.html"></a>
+            </button>
+        </main>
+    </BrowserRouter>
 );
 
 export default root;
